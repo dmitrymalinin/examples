d6a23fa36a9b05c498d1c277f3bfaade1646817f
dm <dm@localhost>
2023-12-23 11:31:17 +0300

    sevfruit-supplier-service: добавлен метод GET /price/{price_id}

--- sevfruit-supplier-service/src/main/java/com/sevfruit/service/PriceController.java
+++ sevfruit-supplier-service/src/main/java/com/sevfruit/service/PriceController.java
@@ -1,11 +1,17 @@
 package com.sevfruit.service;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,20 +31,47 @@ import com.sevfruit.repo.PriceRepository;
 @RestController
 @RequestMapping(path = "/price")
 public class PriceController {
+	private static final Logger logger = LoggerFactory.getLogger(PriceController.class);
+	
+	@Autowired
+	private MessageSource msgSrc;
+	
 	@Autowired
 	private PriceRepository priceRepository;
+	
 	@Autowired
 	private PriceProductRepository priceProductRepository;
 	
 	/**
 	 * Список всех прайс листов <br/>
-	 * {@code  curl -H "Api-Key: 12345" http://localhost:8080/price | jq}
+	 * {@code curl -H "Api-Key: 12345" http://localhost:8080/price | jq}
 	 * @return
 	 */
 	@GetMapping("")
 	public List<Price> getAll()
 	{
 		return priceRepository.findAll();
+	}
+	
+	/**
+	 * Заданный прайс лист <br/>
+	 * {@code curl H "Accept-Language: ru" -H "Api-Key: 12345" http://localhost:8080/price/1 | jq}
+	 * @param price_id
+	 * @param locale
+	 * @return
+	 */
+	@GetMapping("{price_id}")
+	public Price get(@PathVariable int price_id, Locale locale)
+	{
+		final Optional<Price> price = priceRepository.findById(price_id);
+		if (price.isPresent())
+		{
+			return price.get();			
+		} else
+		{
+			logger.error("PriceController.get(): price {} not found", price_id);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, msgSrc.getMessage(ServiceMessages.ERROR_PRICE_NOT_FOUND, new Integer[]{price_id}, locale));
+		}
 	}
 	
 	/**
--- sevfruit-supplier-service/src/main/java/com/sevfruit/service/ServiceMessages.java
+++ sevfruit-supplier-service/src/main/java/com/sevfruit/service/ServiceMessages.java
@@ -9,4 +9,5 @@ public interface ServiceMessages {
 	String ERROR_PERIOD_NOT_FOUND = "error.period_not_found";
 	String ERROR_PRODUCT_NOT_FOUND = "error.product_not_found";
 	String ERROR_SUPPLIER_NOT_FOUND = "error.supplier_not_found";
+	String ERROR_PRICE_NOT_FOUND = "error.price_not_found";
 }
